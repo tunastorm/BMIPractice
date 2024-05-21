1cb5d379799c188205e2339eafaad30ce38ad5e6
tunastorm <ycw921@naver.com>
2024-05-21 16:08:09 +0900

    Initial Commit

---
+++ BMIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIPractice
+//
+//  Created by 유철원 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
